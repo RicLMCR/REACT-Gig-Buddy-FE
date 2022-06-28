7d03514374a064c9815aa90a99167a99b3dc83c6
wumpfroot <markuskojo@msn.com>
2022-06-28 18:59:36 +0100

    Added ugly nav with basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Navbar from './components/navbar';
 
 
 function App() {
 
   return (
     <div className="App">
-      <h1>Gig Buddy</h1>
+      <Navbar />
     </div>
   );
 }

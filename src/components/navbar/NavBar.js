4d51d873f0d82a5fc4653a053942e1c578160e2e
wumpfroot <markuskojo@msn.com>
2022-06-29 15:26:27 +0100

    Added comments to Navbar js and css

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,3 +1,4 @@
+//Importing the CSS-file, with the react icons
 import './navbar.css'
 import { BiSearchAlt } from 'react-icons/bi';
 import { MdMessage } from 'react-icons/md';
@@ -7,12 +8,17 @@ const Navbar = () => {
     return (
     <header>
             <h1>Gig Buddy</h1>
+
+            {/* Calling the search icon from React icons */}
             <BiSearchAlt size={25} className='search-icon' />
+
+            {/* Basic input bar */}
             <input type='text' placeholder="Search" />
         <nav>
             <ul>
-                <li><MdMessage className='message-icon' color='white' /></li>
-                <li></li>
+                {/* Calling the message icon from React icons */}
+                <li><MdMessage size={40} className='message-icon' color='white' /></li>
+                {/* Placeholder image that will be the user profile picture */}
                 <li><img src='https://www.fillmurray.com/40/40' className='nav-profile-pic' alt='Murray' /></li>
             </ul>
             <button className='logout-btn'>Log Out</button>

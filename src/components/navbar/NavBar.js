6a9f56bd4aa6b59a5b006d35e7b2f5a41c69eb27
wumpfroot <markuskojo@msn.com>
2022-06-29 11:27:06 +0100

    Added React icons library

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,9 +1,11 @@
 import './navbar.css'
+import { BiSearchAlt } from 'react-icons/bi';
 
 const Navbar = () => {
     return (
     <header>
             <h1>Gig Buddy</h1>
+            <BiSearchAlt size={25} className='search-icon' />
             <input type='text' placeholder="Search" />
         <nav>
             <ul>

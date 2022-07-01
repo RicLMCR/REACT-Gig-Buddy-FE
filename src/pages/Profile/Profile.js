bd43cad8a22eebb57098f986ea03095d7ef014f2
Jessica Fenna <jessicafenna@gmail.com>
2022-07-01 16:01:47 +0100

    text

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -10,10 +10,10 @@ return (
         <section className='profile-page-top'>
             <div className='profile-picture-div'>
                 <img className='profile-image' src='https://www.fillmurray.com/338/338' alt='profile' />
-                <h1 className='text'>Username</h1>
+                <h1 className='text center'>Username</h1>
             </div>
             <div className='profile-buddies'>
-                <h2 className='buddies-header'>Buddies</h2>
+                <h2 className='text2 buddies-header'>Buddies</h2>
                 <div className='buddy-images'>
                     <img className='buddy-image' src='https://www.fillmurray.com/136/136' alt='buddy1' />
                     <img className='buddy-image' src='https://www.fillmurray.com/136/136' alt='buddy2' />
@@ -25,13 +25,13 @@ return (
         </section>
         <section className="profile-page-bottom">
             <div className='profile-bio'>
-                <h2 className='bio-header'>Profile</h2>
+                <h2 className='bio-header text2'>Profile</h2>
                 <div className='bio-container'>
                     <p className='bio-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci est dolor temporibus quos minima, sed consequatur! Accusantium facere, labore ipsam, quas voluptates sit provident minima expedita sapiente hic consequuntur voluptatem.</p>
                 </div>
             </div>
             <div className='mygigs-div'>
-                <h2 className='mygigs-header'>My Gigs</h2>
+                <h2 className='text2 mygigs-header'>My Gigs</h2>
                 <div className="gig-images">
                 <img className="mygig-image" src="https://www.fillmurray.com/194/194" alt="gig" />
                 <img className="mygig-image" src="https://www.fillmurray.com/194/194" alt="gig" />

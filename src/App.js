9264c9ac1c50ff0251f84c3eb2944d28e319d434
Stalius4 <deivydasrimgaila@gmail.com>
2022-07-05 14:44:18 +0100

    login css fixed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {EventList} from "./components/eventList/eventList";
 
 import {fetchEvents, fetchArtist, trendingEvent} from "./utils/fetchReq"
 
-import Home from './pages/Home';
+
 import Messages from './pages/Messages';
 import Profile from './pages/Profile/Profile';
 import { PopularEvents } from './components/popularEvents/popularEvents';
@@ -48,7 +48,7 @@ const [attendees, setAttendees]=useState([]);
     <>
       <Navbar displayEvent={displayEvent} setDisplayEvent={setDisplayEvent} value={value} setValue={setValue} apiData={apiData} />
       <Routes>
-        <Route path="/" element={ <Home  />} />
+        <Route path="/" element={ <LogOrSign  />} />
         <Route path="/messages" element={ <Messages />}  />
         <Route path="/profile" element={ <Profile  />}  />
         {/* <Route path="/login" element={ <LogOrSign />}  /> */}
--- src/components/LogorSign/LogorSign.js
+++ src/components/LogorSign/LogorSign.js
@@ -3,12 +3,12 @@ import './LogorSign.css'
 import { createUser, logInUser, deleteUser} from '../../utils/fetchReq'
 
 //login or sign up user
-export const LogOrSign = ({setUser}) => {
-
+export const LogOrSign = () => {
+    const [user, setUser]=useState();
     const [username, setUserName] = useState();
     const [email, setEmail] = useState();
     const [password, setPassword] = useState();
-    
+    const [imageUrl, setImageUrl] = useState("steve");
     const submitHandlerCreate=(e)=>{
         e.preventDefault();
         createUser(username, email, password, setUser);
@@ -22,38 +22,46 @@ export const LogOrSign = ({setUser}) => {
     try { 
         // switch between log in page and sign up page
         const [logSwitch, setlogSwitch] = useState(true);
-        return(
+        return(<>
+            {user ? <div>
+                <h1>Gig Buddy</h1>
+                <h1>{user}</h1>
+                <DeleteUser user={user} setUser={setUser}/>
+                <LogOut user={user} setUser={setUser}/>
+            
+                {/* <BuddySwipe/> */}
+            </div>
+            :
             <div>
                 { logSwitch ? 
                 <div className="formContainer">
-               <form className="logIn" onSubmit={submitHandlerLogin}>
+               <form className="login-form" onSubmit={submitHandlerLogin}>
                     <h1 className="text">Log In</h1>
-                    <input placeholder="Username" onChange={(e)=>setUserName(e.target.value)}/>
-                    <input classname = "input" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>                  
+                    <input placeholder="Username" className="logorsign-input" onChange={(e)=>setUserName(e.target.value)}/>
+                    <input className = "logorsign-input" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>                  
                     <button className="loginButton">Submit</button>
-                    <div className="flex">
-                        <h3 className="smallText">Don't have an account?</h3>
-                        <button className = "switchButton" onClick={(e)=>{setlogSwitch()}}>Sign Up</button>
-                    </div>
+                    <h3 className="smallText">Don't have an account?</h3>
+                    <button className = "loginButton" onClick={(e)=>{setlogSwitch()}}>Sign Up</button>
+                   
                 </form>
                 </div>
                 :
                 <div className="formContainer">
-                <form className="signUp" onSubmit={submitHandlerCreate}>
+                <form className="login-form" onSubmit={submitHandlerCreate}>
                     <h1 className="text">Create Profile</h1>
-                    <input placeholder="Username" onChange={(e)=>setUserName(e.target.value)}/>
-                    <input classname = "input" placeholder="Email" onChange={(e)=>setEmail(e.target.value)}/>
-                    <input placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
+                    <input placeholder="Username" className="logorsign-input" onChange={(e)=>setUserName(e.target.value)}/>
+                    <input className="logorsign-input" placeholder="Email" onChange={(e)=>setEmail(e.target.value)}/>
+                    <input className="logorsign-input" placeholder="Password" onChange={(e)=>setPassword(e.target.value)}/>
                     <button className="loginButton" type="submit">Submit</button>
-                    <div className="flex">
+                   
                         <h3 className="smallText">Already have an account?</h3>                
                         <button className = "switchButton"onClick={(e)=>{setlogSwitch(true)}}>Log In</button>
                        
-                    </div>
+                  
                 </form>
                 </div>
                 }
-            </div> 
+            </div>} </>
         )
     } catch (error){ 
         console.log (error)

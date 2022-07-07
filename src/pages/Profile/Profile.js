82d6ce0f5eeea4ba92b87b7aacaf3cb698ec3b78
Luka <lukafrance@gmail.com>
2022-07-07 15:54:03 +0100

    profile-page-css-update

--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -27,19 +27,22 @@ return (
 
 
     <>
+   
     <div className='profile-page-container'>
-
+        <div></div>
         <section className='profile-page-top'>
             <div className='profile-picture-div'>
-            <Avatar alt="Profile" src={user.imageUrl} sx={{ width: 200, height: 200 }} />
+            <Avatar className="avatar-box" alt="Profile" src={user.imageUrl} sx={{ width: 250, height: 250 }} />
             <form onSubmit={submitHandlerLogin}>
-    <input type="text" placeholder="Upload image url" onChange={(e)=>setImageUrl(e.target.value)} name='profileImage' />
-    <button type="Submit"  >Submit</button>
+    <input className="image-upload-box" type="text" placeholder="Upload image url" onChange={(e)=>setImageUrl(e.target.value)} name='profileImage' />
+    <button   className="submit-btn"type="Submit"  >Submit</button>
+  
     </form>
             </div>
             <div className='profile-buddies'>
-                <h2 className='buddies-header'>Buddies</h2>
+              
                 <div className='buddy-images'>
+                <h2 className='buddies-header'>Buddies</h2>
                     <img className='buddy-image' src='https://www.fillmurray.com/136/136' alt='buddy1' />
                     <img className='buddy-image' src='https://www.fillmurray.com/136/136' alt='buddy2' />
                     <img className='buddy-image' src='https://www.fillmurray.com/136/136' alt='buddy3' />
@@ -50,8 +53,9 @@ return (
         </section>
         <section className="profile-page-bottom">
             <div className='profile-bio'>
-                <h2 className='bio-header'>Profile</h2>
+
                 <div className='bio-container'>
+                    <h2>Profile</h2>
                     <p className='bio-text'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci est dolor temporibus quos minima, sed consequatur! Accusantium facere, labore ipsam, quas voluptates sit provident minima expedita sapiente hic consequuntur voluptatem.</p>
                 </div>
             </div>
